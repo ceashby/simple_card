90a5e0a3a0808e4e37b717dd3976240d275e3cc8
Chris Ashby <crrashby@gmail.com>
2017-03-26 21:32:33 +0100

    no message

--- simple_card/wsgi.py
+++ simple_card/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_card.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_card.production_settings")
 
 application = get_wsgi_application()
